ff3ca8438727f9c290bfcb88a554eae861d20a82
Alcuavi <alberto.cuvi@gmail.com>
2024-10-08 10:19:54 +0200

    Fix: prevent loading .env in production

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,19 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Solo carga el archivo .env en los entornos de desarrollo o testing
+        if ($this->getEnvironment() === 'dev' || $this->getEnvironment() === 'test') {
+            (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+        }
+    }
 }
